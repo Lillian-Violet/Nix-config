3fb9b478765c68afaa8c98745d75033c3926918c
Lillian Violet <git@gladtherescake.eu>
2023-10-16 11:30:54 +0200

    Added home manager configuration to flake

--- flake.nix
+++ flake.nix
@@ -6,16 +6,35 @@
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations = {
-      yourHostNameGoesHere = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-        ];
-      };
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    EDI = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hardware-configuration.nix
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          # Pin nixpkgs to the version used to build the system
+          nix.registry.nixpkgs.flake = nixpkgs;
+
+          home-manager = {
+            useGlobalPkgs = true; # <--------------------------------
+            useUserPackages = true;
+            users = let
+              homeCfg = import ./home.nix;
+            in
+              {
+                root = homeCfg;
+                malte = homeCfg;
+              };
+          };
+        }
+      ];
     };
   };
 }
