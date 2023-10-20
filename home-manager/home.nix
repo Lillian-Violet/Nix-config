28b0bd98f2a08e889006dfa3087727e1ac290d1d
Lillian Violet <git@gladtherescake.eu>
2023-10-20 16:29:37 +0200

    use vscodium instead of vscode

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -113,6 +113,7 @@
 
   programs.vscode = {
     enable = true;
+    package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
       arrterian.nix-env-selector
