b254e98f5817bb045b64d75e3b9da6c7f515f988
Lillian Violet <git@gladtherescake.eu>
2023-10-20 23:56:04 +0200

    I'm genuinely baffled

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -21,7 +21,7 @@
     # ./users.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
-    ./nixos/hardware-configuration.nix
+    ./hardware-configuration.nix
     inputs.home-manager.nixosModules.home-manager
   ];
 
